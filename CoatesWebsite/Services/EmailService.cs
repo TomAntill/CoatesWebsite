eae808df506a65af063e1097879b50c1f1a32c39
Thomas Antill <thomasantill92@gmail.com>
2023-05-04 22:08:00 +0100

    Email

--- CoatesWebsite/Services/EmailService.cs
+++ CoatesWebsite/Services/EmailService.cs
@@ -3,20 +3,24 @@ using System;
 using System.Threading.Tasks;
 using SendGrid;
 using SendGrid.Helpers.Mail;
-
+using Microsoft.Extensions.Configuration;
 
 namespace CoatesWebsite.Data.Services
 {
     public class EmailService : IEmailService
     {
         private readonly CoatesContext _context;
-        public EmailService(CoatesContext context)
+        private readonly IConfiguration configuration;
+
+        public EmailService(CoatesContext context, IConfiguration configuration)
         {
             _context = context;
+            this.configuration = configuration;
         }
+
         public async Task<int> Add(Email email)
         {
-            var apiKey = Environment.GetEnvironmentVariable("SEND_GRID_API_KEY");
+            var apiKey = configuration.GetSection("SEND_GRID_API_KEY").Value;
             var client = new SendGridClient(apiKey);
             var from = new EmailAddress("info@coatescarpentry.co.uk", email.EmailAddress);
             var to = new EmailAddress("info@coatescarpentry.co.uk", "Tom");
