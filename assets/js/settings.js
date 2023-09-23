b4c0da8c5817a2b49402cebf488944458d07df25
brutt-dev <ruttben@yahoo.com>
2023-09-23 15:39:18 +0100

    ben gallery fixes

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -3,16 +3,26 @@ const WEBSITE_SETTINGS = {
   WEBSITE_ID: 0,
   UNSET_WEBSITE_ID: 99,
   BACKEND_SERVICES_BASE_PATH: "https://localhost:44311/",
-  BACKEND_SERVICES_JS_PATH: "assets/js/websites/services/modules/",
+  BACKEND_SERVICES_JS_PATH: "assets/js/websites/",
 };
 
-function postLoadScript(file, isModule, appendToHead) {
+function postLoadScript(file, isModule, appendToHead, callback) {
   const scriptElement = document.createElement("script");
-  scriptElement.src = `${WEBSITE_SETTINGS.BACKEND_SERVICES_BASE_PATH}${WEBSITE_SETTINGS.BACKEND_SERVICES_JS_PATH}${file}`;
-  console.log("bb1", scriptElement.src);
+  scriptElement.src = `${WEBSITE_SETTINGS.BACKEND_SERVICES_BASE_PATH}${
+    WEBSITE_SETTINGS.BACKEND_SERVICES_JS_PATH
+  }${isModule ? `services/modules/` : ``}${file}`;
   if (isModule) {
     scriptElement.type = "module";
   }
+
+  if (callback != null) {
+    scriptElement.onload = () => {
+      if (typeof callback === "function") {
+        callback();
+      }
+    };
+  }
+
   if (appendToHead) {
     document.head.appendChild(scriptElement);
   } else {
